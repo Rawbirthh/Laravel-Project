012d0114f212f3e1fe4a835d194a3878afbbd764
Rawbirthh <robertbarrios456@gmail.com>
2026-06-24 09:02:17 +0800

    redeploy

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ set -e
 
 # 1. Run migrations and seeders
 php artisan migrate --force
-#php artisan db:seed --class=AdminSeeder --force
+##php artisan db:seed --class=AdminSeeder --force
 
 # 2. Run the webdevops entrypoint to set up Nginx/PHP, then start Supervisord
 exec /entrypoint supervisord
